5f4e52fcf0866f14bbc068c5dada5b65c1e06f77
RohitGoswami5693 <rohitgoswami5693@gmail.com>
2024-06-02 16:39:06 +0530

    Add files via upload

---
+++ 01_environment.js
@@ -0,0 +1,6 @@
+// Setting up environment in local machine for JS     ==>
+// ==============================================
+
+
+    // NodeJS, DenoJS {Founder -> Ryan Dahl} --> JS running environments
+
